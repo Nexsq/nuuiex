e6cfa0dddc92afb9c37aaede7fd0ad0cb230d2e2
Nexsq <alexge2232@gmail.com>
2026-04-26 00:22:19 +0200

    getting started, this one bouta be fire

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod canvas;
+use canvas::Canvas;
+use crossterm::{
+    cursor::MoveTo,
+    event::{poll, read, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+    ExecutableCommand,
+};
+use std::{io::stdout, time::Duration};
+
 fn main() {
-    println!("Hello, nex");
+    enable_raw_mode().unwrap();
+
+    let mut canvas = Canvas::new();
+
+    loop {
+        canvas.clear();
+        canvas.draw_box((10, 5), (20, 10), 1);
+        canvas.draw_box((20, 8), (15, 8), 1);
+        canvas.render();
+
+        if poll(Duration::from_millis(50)).unwrap() {
+            match read().unwrap() {
+                Event::Resize(cols, rows) => {
+                    canvas.resize(cols, rows);
+                }
+                Event::Key(key_event) => {
+                    if key_event.code == KeyCode::Esc {
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    disable_raw_mode().unwrap();
+    let mut stdout = stdout();
+    stdout.execute(MoveTo(0, canvas.height)).unwrap();
 }
